236a65b00b7e1696c4b8b03e4accc86cccd976d0
rizal <rizal.rifqiana@lintasarta.co.id>
2025-06-26 18:41:51 +0700

    ubah folder hasil automate

--- preprocessing/automate_Rizal.py
+++ preprocessing/automate_Rizal.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-def preprocess_iris(input_csv='iris_raw.csv', output_csv='preprocessing/iris_preprocessing.csv'):
+def preprocess_iris(input_csv='iris_raw.csv', output_csv='iris_preprocessing.csv'):
     df = pd.read_csv(input_csv)
     le = LabelEncoder()
     df['species'] = le.fit_transform(df['species'])
